00499f930d96ebd292880611e65c8c83611a8ad7
BossNumber1 <dimaang23@gmail.com>
2022-06-07 20:08:44 +0300

    Added a frame to the main page

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -4,7 +4,31 @@ import '../styles/App.css';
 function App() {
   return (
     <div className="App">
-      <h1>application</h1>
+      <div className="header">
+        Готовые проекты
+      </div>
+      <div className="main">
+        <div className="main__row">
+          <div className="main__column">
+            1й столбец
+          </div>
+          <div className="main__column">
+            2й столбец
+          </div>
+          <div className="main__column">
+            3й столбец
+          </div>
+        </div>
+        <div className="main__row">
+          2й ряд
+        </div>
+        <div className="main__row">
+          3й ряд
+        </div>
+      </div>
+      <div className="footer">
+        Ссылки
+      </div>
     </div>
   );
 }
