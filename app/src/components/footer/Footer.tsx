5f5f4b4a0271d638a377abfc17fa3e95e4640337
BossNumber1 <dimaang23@gmail.com>
2022-06-08 13:33:59 +0300

    added the linkIcons + mixins

--- app/src/components/footer/Footer.tsx
+++ app/src/components/footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import vkIcon from '../../pictures/vkIcon.svg'
+import tgIcon from '../../pictures/tgIcon.svg'
+import gmailIcon from '../../pictures/gmailIcon.svg'
 
 function Footer() {
     return (
         <div className="footer">
-            Ссылки
+            <div className='footer__linkIcon'>
+                <img src={vkIcon} alt="vk icon" />
+            </div>
+            <div className='footer__linkIcon'>
+                <img src={tgIcon} alt="tg icon" />
+            </div>
+            <div className='footer__linkIcon'>
+                <img src={gmailIcon} alt="gmail icon" />
+            </div>
         </div>
     );
 }
